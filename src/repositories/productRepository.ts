9446b985da701d1008318a7e09b7797122cc61fb
Larissa Paloze <lpaloze@gmail.com>
2026-05-24 12:48:29 -0300

    adicionei operações CRUD no productRepository

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -10,4 +10,16 @@ export const productRepository = {
     return await productsCollection.findOne({ id });
   },
 
-}
+  async create(product: any) {
+    return await productsCollection.insertOne(product);
+  },
+
+  async update(id: number, data: any) {
+    return await productsCollection.updateOne({ id }, { $set: data });
+  },
+
+  async delete(id: number) {
+    return await productsCollection.deleteOne({ id });
+  },
+
+};
