449ccfc91f848cdf7bd6f99e160f146ef6ada3d7
Larissa Paloze <lpaloze@gmail.com>
2026-05-24 12:55:24 -0300

    adicionei criação de produtos

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,18 +4,40 @@ let nextId = 1;
 
 export const productService = {
 
-  async list() {
-    return await productRepository.findAll();
-  },
+    async create(data: any) {
+        const { nome, materiais } = data;
 
-  async getById(id: number) {
-    const product = await productRepository.findById(id);
+        if (!nome || !Array.isArray(materiais) || materiais.length === 0) {
+            throw new Error(
+                "nome e materiais (array não vazio) são obrigatórios"
+            );
+        }
 
-    if (!product) {
-      throw new Error("Produto não encontrado");
-    }
+        await productRepository.validateMaterials(materiais);
 
-    return product;
-  },
+        const product = {
+            id: nextId++,
+            nome,
+            materiais,
+        };
+
+        await productRepository.create(product);
+
+        return product;
+    },
+
+    async list() {
+        return await productRepository.findAll();
+    },
+
+    async getById(id: number) {
+        const product = await productRepository.findById(id);
+
+        if (!product) {
+            throw new Error("Produto não encontrado");
+        }
+
+        return product;
+    },
 
 };
