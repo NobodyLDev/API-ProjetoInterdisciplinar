65035eee1036c63728c134beb6edae7ced2de12d
NobodyLDev <leticiaschiffel11@gmail.com>
2026-05-26 14:42:40 -0300

    correção index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { createServer } from "./server";
+import { start } from "./server";
 
-const app = createServer();
-const port = Number(process.env.PORT || 3000);
-
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+start().catch((error) => {
+  console.error("Falha ao iniciar a API:", error);
+  process.exit(1);
 });
