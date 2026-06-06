989537974d96aa4a9448e339b3259f1dd0653160
Nicolas Masaishi Oi Pessoa <masaishi.pessoa@gmail.com>
2026-06-06 19:47:47 -0300

    feat: adicionar historyCollection ao database e exportar para repositório.

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,8 @@
-// src/database.ts
 import dotenv from "dotenv";
 import { MongoClient, Collection } from "mongodb";
 import path from "path";
 import { config } from "../config/config";
 
-// Carrega o .env a partir do diretório de trabalho do processo (raiz do projeto)
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 const mongoUri = config.mongoUri;
@@ -16,12 +14,13 @@ if (!mongoUri) {
 }
 
 const safeUri = mongoUri.replace(/(mongodb\+srv:\/\/[^:]+):[^@]+@/, "$1:****@");
-console.log("Tentando conectar com URI:", safeUri); // Log sem senha
+console.log("Tentando conectar com URI:", safeUri);
 
 const client = new MongoClient(mongoUri);
 
 export let materialsCollection: Collection<any>;
 export let productsCollection: Collection<any>;
+export let historyCollection: Collection<any>;
 
 export async function connectDatabase(): Promise<void> {
   try {
@@ -30,6 +29,7 @@ export async function connectDatabase(): Promise<void> {
 
     materialsCollection = db.collection("materials");
     productsCollection = db.collection("products");
+    historyCollection = db.collection("history");
 
     console.log(`MongoDB conectado em ${dbName}`);
   } catch (error) {
