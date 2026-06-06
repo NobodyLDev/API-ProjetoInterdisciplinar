f60e686880ae7398b69ac8627511514aa9cf1cb6
Nicolas Masaishi Oi Pessoa <masaishi.pessoa@gmail.com>
2026-06-06 19:49:42 -0300

    feat: implementar historyRepository conectado ao MongoDB.

--- src/repositories/historyRepository.ts
+++ src/repositories/historyRepository.ts
@@ -1,20 +1,36 @@
+import { ObjectId } from "mongodb";
+import { historyCollection } from "../database/database";
 import { History } from "../models/history";
 
 export class HistoryRepository {
-  async create(data: History) {
-    /*
-      Implementação do banco ficará
-      responsável pela equipe de infraestrutura.
-    */
 
-    return data;
+  async create(data: History) {
+    const result = await historyCollection.insertOne({
+      ...data,
+      createdAt: new Date(),
+    });
+
+    return {
+      _id: result.insertedId,
+      ...data,
+    };
   }
 
   async findAll() {
-    /*
-      Implementação futura do banco.
-    */
-
-    return [];
+    return await historyCollection
+      .find()
+      .sort({ createdAt: -1 })
+      .toArray();
   }
+
+  async findById(id: string) {
+    if (!ObjectId.isValid(id)) throw new Error("ID inválido");
+    return await historyCollection.findOne({ _id: new ObjectId(id) });
+  }
+
+  async delete(id: string) {
+    if (!ObjectId.isValid(id)) throw new Error("ID inválido");
+    return await historyCollection.deleteOne({ _id: new ObjectId(id) });
+  }
+
 }
