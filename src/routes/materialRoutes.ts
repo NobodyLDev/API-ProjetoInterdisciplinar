e3e8ddbf75b44197433e7b8e22b9417570969cf2
Nicolas Masaishi Oi Pessoa <masaishi.pessoa@gmail.com>
2026-06-06 19:55:26 -0300

    feat: adicionar rotas GET por id, PUT e DELETE no materialRoutes.

--- src/routes/materialRoutes.ts
+++ src/routes/materialRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-
 import { materialController } from "../controllers/materialControllers";
 
 const router = Router();
 
 router.get("/", materialController.list);
-
+router.get("/:id", materialController.getById);
 router.post("/", materialController.create);
+router.put("/:id", materialController.update);
+router.delete("/:id", materialController.delete);
 
 export default router;
